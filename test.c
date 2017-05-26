832864c7201e524d2cb56ecd573e2561f9031c38
infinitapple <infinitapple@hanmail.net>
2017-05-26 11:09:42 +0900

    Update test.c

--- test.c
+++ test.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #define ROOP 10
 int main(void) {
+  char arry;
   for(int i=0; i<ROOP; i++)
   printf("Hello World!");
+  
+  scanf("%s",arry);
+  
+  cout<<arry;
   
   return 0;
 }
